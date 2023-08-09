80422f921aa325082dab0cf84d4d8ac818dde74e
manfredjoa <joamanfred@gmail.com>
2023-08-09 10:35:25 -0400

    add singlyLinkedLists

---
+++ singlyLinkedLists/script.js
@@ -0,0 +1,60 @@
+class Node {
+  constructor(value) {
+    this.val = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) {
+      return undefined;
+    }
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
+}
+
+const list = new SinglyLinkedList();
+list.push("A");
+list.push("B");
+list.push("C");
+list.pop();
+list.pop();
+list.pop();
+console.log(list);
